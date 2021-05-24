4b8177514c6ec816fc840ccef0e2e63bccc0bc15
张俊杰 <1324523256@qq.com>
2021-05-24 16:00:06 +0800

    添加影评数据

--- Douban_Movies_Analysic-master/数据etl/影评预处理并存储至hive表中/yingpingETL.py
+++ Douban_Movies_Analysic-master/数据etl/影评预处理并存储至hive表中/yingpingETL.py
@@ -11,8 +11,8 @@
 #
 # fp_w.close()
 # print("OK !")
-fp_w = open("new_1291858.txt", "a", encoding="utf-8")
-with open("1291858 .txt", "r", encoding="utf-8") as fp:
+fp_w = open("new_1291561.txt", "a", encoding="utf-8")
+with open("1291561 .txt", "r", encoding="utf-8") as fp:
     for line in fp.readlines():
         line_list = line.split("\t")
         if len(line_list)==6:
